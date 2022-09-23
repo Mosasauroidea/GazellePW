dddd798696900c32b80d8456d240ef315f4b642d
NoobMaster69 <NoobMaster669@protonmail.com>
2022-09-23 19:37:26 +0530

    supporting search by imdb and returning all applicable torrents

--- app/API/Torrent.php
+++ app/API/Torrent.php
@@ -7,70 +7,95 @@ class Torrent extends AbstractAPI {
         switch ($_GET['req']) {
             case 'group':
                 return $this->getGroup();
-                break;
-            default:
             case 'torrent':
                 return $this->getTorrent();
-                break;
+            default:
+                return $this->getTorrent(); 
         }
     }
 
     private function getTorrent() {
-        if (!isset($_GET['torrent_id'])) {
-            json_error('Missing torrent id');
+        if (isset($_GET['torrent_id'])) {
+            $this->db->prepared_query("
+                SELECT
+                    tg.ID,
+                    tg.Name,
+                    tg.Year,
+                    tg.ReleaseType AS ReleaseTypeID,
+                    t.Codec,
+                    t.Resolution,
+                    t.Container,
+                    t.Processing,
+                    t.Source,
+                    tls.Snatched,
+                    tls.Seeders,
+                    tls.Leechers
+                FROM
+                    torrents AS t
+                    INNER JOIN torrents_leech_stats tls ON (tls.TorrentID = t.ID)
+                    INNER JOIN torrents_group AS tg ON (tg.ID = t.GroupID)
+                WHERE
+                    t.ID = ?", $_GET['torrent_id']);
+        } elseif(isset($_GET['imdbID'])){
+            $this->db->prepared_query("
+                SELECT
+                    tg.ID,
+                    tg.Name,
+                    tg.Year,
+                    tg.IMDBID,
+                    tg.ReleaseType AS ReleaseTypeID,
+                    t.Codec,
+                    t.Resolution,
+                    t.Container,
+                    t.Processing,
+                    t.Source
+                FROM
+                    torrents_group AS tg
+                    INNER JOIN torrents AS t ON (tg.ID = t.GroupID)
+                WHERE
+                    tg.IMDBID = ?", $_GET['imdbID']);
+        } else {
+            json_error('Missing torrent id and imdbid');
         }
 
-        $this->db->prepared_query("
-            SELECT
-                tg.ID,
-                tg.Name,
-                tg.Year,
-                tg.ReleaseType AS ReleaseTypeID,
-                t.Codec,
-                t.Resolution,
-                t.Container,
-                t.Processing,
-                t.Source,
-                tls.Snatched,
-                tls.Seeders,
-                tls.Leechers
-            FROM
-                torrents AS t
-                INNER JOIN torrents_leech_stats tls ON (tls.TorrentID = t.ID)
-                INNER JOIN torrents_group AS tg ON (tg.ID = t.GroupID)
-            WHERE
-                t.ID = ?", $_GET['torrent_id']);
         if (!$this->db->has_results()) {
             json_error('Torrent not found');
         }
-        $torrent = $this->db->next_record(MYSQLI_ASSOC, false);
-        $torrent['ReleaseType'] = $torrent['ReleaseTypeID'];
-        $artists = \Artists::get_artist($torrent['ID']);
-        $torrent['Artists'] = $artists;
-        $torrent['DisplayArtists'] = \Artists::display_artists(
-            $artists,
-            false,
-            false,
-            false
-        );
-        return $torrent;
+        $response = array();
+        for ($x = 0; $x < $this->db->record_count(); $x++) {
+            $torrent = $this->db->next_record(MYSQLI_ASSOC, false);
+            array_push($response, $torrent);
+        }
+        return $response;
     }
 
     private function getGroup() {
-        if (!isset($_GET['group_id'])) {
-            json_error('Missing group id');
+        if(isset($_GET['group_id'])){
+            $this->db->prepared_query("
+                SELECT
+                    ID,
+                    Name,
+                    Year,
+                    ReleaseType AS ReleaseTypeID
+                FROM
+                    torrents_group
+                WHERE
+                    ID = ?", $_GET['group_id']);
+        } elseif (isset($_GET['imdbID'])){
+            $this->db->prepared_query("
+                SELECT
+                    ID,
+                    Name,
+                    Year,
+                    ReleaseType AS ReleaseTypeID
+                FROM
+                    torrents_group
+                WHERE
+                    IMDBID = ?", $_GET['imdbID']);
+        } else {
+            json_error('Missing group id and imdbid');
         }
-
-        $this->db->prepared_query("
-            SELECT
-                ID,
-                Name,
-                Year,
-                ReleaseType AS ReleaseTypeID
-            FROM
-                torrents_group
-            WHERE
-                ID = ?", $_GET['group_id']);
+        
         if (!$this->db->has_results()) {
             json_error('Group not found');
         }
