f8281aac1914bdefa4e82f99921081a32b7ec13a
NoobMaster69 <NoobMaster669@protonmail.com>
2022-09-23 19:38:56 +0530

    providing db support for apis

--- public/api.php
+++ public/api.php
@@ -16,6 +16,7 @@ if (isset($_GET['clearcache'])) {
 }
 require_once(__DIR__ . '/../classes/classloader.php');
 require_once(__DIR__ . '/../classes/config.php');
+require_once(__DIR__ . '/../classes/mysql.class.php');
 require_once(__DIR__ . '/../classes/const.php');
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../classes/util.php');
